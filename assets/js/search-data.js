b6059f30fb6869f453140ff211ff7eb99842b619
MorbidRose916 <183243152+MorbidRose916@users.noreply.github.com>
2025-07-31 19:14:43 +0000

    Deploying to gh-pages from @ MorbidRose916/morbidrose916.github.io@5a0d3e5aa8048bd92c536e1ac88f79135d9251a3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,16 +37,9 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/repositories/";
           },
-        },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
+        },{id: "nav-learning",
+          title: "learning",
+          description: "Materials for courses you have completed. Replace this text with your description.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
